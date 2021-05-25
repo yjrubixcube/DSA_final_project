c3637df8c57c3db71cc030a89454b4f1f7a68d19
unknown <dickjohnson8877@gmail.com>
2021-05-25 18:26:38 +0800

    add dickface

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(){
     printf("ABC\n");
-    printf("hello world");
-    printf("I am power incarnate");
+    printf("hello world\n");
+    printf("I am power incarnate\n");
+    printf("I'm dickface.\n");
     return 0;
 }
