b4e64fdf65b90eda536262d261ddbdd1eb2caa25
yjrubixcube <80613269+yjrubixcube@users.noreply.github.com>
2021-06-22 15:20:14 +0800

    added hash and find_node

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include "api.h"
 #include <stdio.h>
-
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
+#define HASH_SIZE 100
 
 int n_mails, n_queries;
 mail *mails;
 query *queries;
-
-//TODO: make a hash
 
 typedef struct dset
 {
@@ -19,15 +17,48 @@ typedef struct dset
 typedef struct dsnode{
 	dset *parent;
 	struct dsnode *next;
+	char *name;
 	//some info
 }dsnode;
 
-dset *find_set(dsnode *node){
+dsnode *hash_table[HASH_SIZE]={NULL};
+
+dsnode *find_node(char *str){
+	unsigned long h = hash(str);
+	while (hash_table[h]!=NULL){
+		if (hash_table[h]->name!=str){
+			h++; //need better probing method
+		}
+		else{
+			return hash_table[h];
+		}
+	}
+}
+
+dset *find_set(char *str){
+	dsnode *node = find_node(str);
+	return node->parent;
 
 }
 
+void init_node(char *str){
+	dsnode *new = (dsnode *)malloc(sizeof(dsnode));
+	new->name = str;
+	new->next = NULL;
+	new->parent = NULL; //what should this be
+}
+
+unsigned long hash(char *str){
+	// google: dbj2
+	unsigned long h = 5381;
+	int c;
+	while (c=*str++){
+		h = ((h<<5)+h)+c; //h*33+c
+	}
+	return h%HASH_SIZE;
+}
+
 void Union(dset *s1, dset *s2){ //by size
-	if ()
 }
 
 int main(void) {
