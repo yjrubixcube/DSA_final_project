d1c7162039acf161e01c4ba2577f512f8e76e934
yjrubixcube <80613269+yjrubixcube@users.noreply.github.com>
2021-06-24 16:50:58 +0800

    temp

--- expression_match.c
+++ expression_match.c
@@ -18,7 +18,34 @@ typedef struct stack
     char *token;
 }stack;
 
-stack* push(stack *stk, int type, bool exp, char *w){
+typedef struct qnode
+{
+    struct qnode *next;
+    int id, type; //0:(, 1:), 2:not, 3:and, 4:or, 5:token_id
+}qnode;
+
+typedef struct queue
+{
+    qnode *head, *tail;
+    int len;
+}queue;
+
+void qpush(queue *q, int type, int id){
+    qnode *new = (qnode *)malloc(sizeof(qnode));
+    new->id = id;
+    new->type = type;
+    if (q->len==0){
+        q->head = new;
+        q->tail = new;
+    }
+    else{
+        q->tail->next = new;
+        q->tail = new;
+    }
+    q->len++;
+}
+
+stack* spush(stack *stk, int type, bool exp, char *w){
     stack *new = (stack*)malloc(sizeof(stack));
     new->type = type;
     new->exp = (type==5) ? exp : false;
@@ -27,7 +54,7 @@ stack* push(stack *stk, int type, bool exp, char *w){
     return new;
 }
 
-stack* pop(stack *stk){
+stack* spop(stack *stk){
 
 }
 
@@ -76,13 +103,23 @@ void insert_token(trie_node *root, char *w){
     int index;
 
     while(w[level] != '\0'){
-        //first character is in uppercase
-        index = w[level] - ((w[level]>='a') ? 'a' : 'A');
+        if (w[level]>='a'){
+            index = w[level]-'a';
+        }
+        else if (w[level]>='A'){
+            index = w[level]-'A';
+        }
+        else if (w[level]>='0'){
+            index = w[level]-'0'+26;
+        }
         if(cur->child[index] == NULL){
             cur->child[index] = build_node();
         }
         cur = cur->child[index];
         level += 1;
+    }
+    if (!cur->is_name){
+        cur->is_name = true;
     }
 }
 
@@ -100,7 +137,7 @@ bool check_token(trie_node *root, char *w, int length){
             index = w[level]-'A';
         }
         else if (w[level]>='0'){
-            index = w[level]-'0';
+            index = w[level]-'0'+26;
         }
 
         if (cur->child[index] == NULL){
@@ -122,25 +159,60 @@ bool check_token(trie_node *root, char *w, int length){
 
 //this adds all the tokens in the expression into trie
 //TODO
-void preprocess(char *exp, trie_node *root, stack *stk){
+//makes a queue of expression
+void preprocess(char *exp, trie_node *root, stack *stk, queue *q){
     int index = 0;
     while(exp[index]!='\0'){
         int length=0;
         while ((exp[index+length]<='z' && exp[index+length]>='A')|| (exp[index+length] <= '9' && exp[index+length] >= '0')){
             length+=1;
-            
         }
         if (length>0){
             char ins[length], temp = exp[index+length];
+            int token_id;
             exp[index+length]='\0';
             strcpy(ins, exp+index);
             insert_token(root, ins);
+            qpush(q, 5, token_id);
             exp[index+length]=temp;
             index+=length;
         }else{
+            switch (exp[index])
+            {
+            case '(':
+                qpush(q, 0, -1);
+                break;
+            
+            case ')':
+                qpush(q, 1, -1);
+                break;
+            
+            case '!':
+                qpush(q, 2, -1);
+                break;
+
+            case '&':
+                qpush(q, 3, -1);
+                break;
+
+            case '|':
+                qpush(q, 4, -1);
+                break;
+            }
             index+=1;
         }
     }
+}
+
+bool eval(queue *q){
+    qnode *cur = q->head;
+    stack *s = NULL;
+    while (cur!=NULL)
+    {
+        if (cur->type==)
+    }
+    
+
 }
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
@@ -161,6 +233,17 @@ int main(){
 	int ans[2];
 	for(int i = 0; i < n_queries; i++){
         if(queries[i].type == expression_match){
+            //make a trie for each mail
+            //eval the expression
+            //for all token
+            char *expression = queries[i].data.expression_match_data.expression;
+            queue *q = (queue *)malloc(sizeof(queue));
+            q->head = NULL;
+            q->tail = NULL;
+            q->len = 0;
+            for (int j = 0;j<n;j++){
+
+            }
 
         }
     }
